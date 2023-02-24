fe46e10199059240fa56e681737e1b6a8a79c747
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-02-24 12:34:31 +0100

    feat: update ui
    add skeleton while loading the notes

---
+++ src/components/wordCardSkeleton.tsx
@@ -0,0 +1,31 @@
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+const WordCardSkeleton = () => {
+  return (
+    <SkeletonTheme baseColor="#1e283b" highlightColor="#334155">
+      <div className="border rounded-md border-gray-500 flex flex-col p-4">
+        {/* heading and body */}
+        <div className="flex flex-col gap-1">
+          {/* heading */}
+          <Skeleton height={"14px"} />
+          <Skeleton width={"50%"} height={"12px"} />
+          <section>
+            <Skeleton count={5} />
+          </section>
+        </div>
+        {/* like, dislike, comment */}
+        <div
+          onClick={() => console.log("reaction button clicked")}
+          className="flex justify-between mt-2 px-4 sm:px-3 md:px-2 z-10"
+        >
+          <Skeleton circle height="20px" width={"20px"} />
+          <Skeleton circle height="20px" width={"20px"} />
+          <Skeleton circle height="20px" width={"20px"} />
+          <Skeleton circle height="20px" width={"20px"} />
+        </div>
+      </div>
+    </SkeletonTheme>
+  );
+};
+
+export default WordCardSkeleton;
--- src/pages/words/index.tsx
+++ src/pages/words/index.tsx
@@ -1,26 +1,35 @@
 import Seo from "@/components/SEO";
 import WordCard from "@/components/wordCard";
+import WordCardSkeleton from "@/components/wordCardSkeleton";
 import WordsLayout from "@/components/wordsLayout";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Words = () => {
   const [strangerWords, setStrangerWords] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const cardSkeleton = [];
+
+  for (let i = 0; i < 12; i++) {
+    cardSkeleton.push(<WordCardSkeleton />);
+  }
 
   useEffect(() => {
     axios.get("https://dagmawibabi.com/wot/getNotes/time/-1").then((res) => {
-      console.log(res.data);
       setStrangerWords(res.data);
+      setLoading(false);
     });
   }, []);
   return (
     <>
       <Seo />
       <WordsLayout>
-        {strangerWords.map((strangerWord: any) => {
+        {loading && cardSkeleton}
+        {strangerWords.map((strangerWord: any, index: number) => {
           return (
             <WordCard
-              key={strangerWord._id}
+              key={index}
               heading={strangerWord.title}
               body={strangerWord.content}
               likes={strangerWord.likes}
