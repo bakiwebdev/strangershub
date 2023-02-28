8afeb57d402ea8785685f338169f38dc45973e41
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-02-28 06:00:33 +0100

    feat: change add post floating

--- src/components/postFilter.tsx
+++ src/components/postFilter.tsx
@@ -14,21 +14,14 @@ const WordFilter = () => {
   }
 
   return (
-    <div className="fixed w-full mx-auto py-2 top-0 z-20 pt-20 bg-slate-900 border-b border-gray-500">
-      {/* filter option */}
-      <div className="px-2 relative max-w-7xl mx-auto flex justify-between">
-        {/* filter options */}
-        <div></div>
-        {/* post words */}
-        <button
-          onClick={openModal}
-          className="flex justify-center items-center px-2 md:px-5 py-2 rounded-full text-slate-800 bg-orange-500 font-semibold"
-        >
-          <span className="hidden md:block">Have something to say ?</span>
-          <PencilIcon className="w-5 h-5 block md:hidden" />
-        </button>
-      </div>
-      {/* modal */}
+    <div className="fixed bottom-10 right-6 md:bottom-20 md:right-14 w-fit h-fit rounded z-40">
+      <button
+        onClick={openModal}
+        className="flex justify-center items-center px-2 md:px-5 py-2 rounded-full text-slate-800 bg-orange-500 font-semibold"
+      >
+        <span className="hidden md:block">Have something to say ?</span>
+        <PencilIcon className="w-7 h-7 block md:hidden" />
+      </button>
       <InputModal isOpen={isOpen} onRequestClose={closeModal} />
     </div>
   );
--- src/components/postLayout.tsx
+++ src/components/postLayout.tsx
@@ -1,13 +1,13 @@
-interface WordsLayoutProps {
+interface PostLayoutProps {
   children: React.ReactNode | React.ReactNode[];
 }
 
-const WordsLayout = ({ children }: WordsLayoutProps) => {
+const PostLayout = ({ children }: PostLayoutProps) => {
   return (
-    <div className="max-w-7xl mx-auto pt-[10rem] px-10 sm:px-8 md:px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
+    <div className="max-w-7xl mx-auto pt-24 px-10 sm:px-8 md:px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
       {children}
     </div>
   );
 };
 
-export default WordsLayout;
+export default PostLayout;
