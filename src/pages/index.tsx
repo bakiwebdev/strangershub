7a16ad400064b40f48839a746aa8f1c2fdb01db2
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-03-01 03:16:11 +0100

    fix: remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 import Seo from "@/components/SEO";
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
