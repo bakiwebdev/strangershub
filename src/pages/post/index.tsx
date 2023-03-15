844ef4477ec99e50443e4970549f261ca1b5ac5a
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-03-15 02:08:30 +0100

    build search and filter component

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -5,6 +5,12 @@ import PostCardSkeleton from "@/components/postCardSkeleton";
 import PostLayout from "@/components/postLayout";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import {
+  BarsArrowDownIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  PencilIcon,
+} from "@heroicons/react/24/outline";
 
 const Post = () => {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
@@ -29,6 +35,32 @@ const Post = () => {
     <>
       <Seo />
       <PostFilter />
+      <div className="flex justify-center items-center max-w-7xl mx-auto mt-28 px-10 gap-2">
+        {/* search size */}
+        <div className="w-full px-5 py-3 rounded-md border border-orange-500/50 shadow-xl shadow-orange-500/20 flex justify-center items-center gap-2">
+          <MagnifyingGlassIcon className="w-5 h-5 text-slate-400" />
+          <input
+            className="bg-transparent w-32 outline-none md:mx-2 flex-1"
+            type="search"
+            placeholder="Search..."
+          />
+          <div className="border border-slate-400 h-4 mx-1 md:mx-2"></div>
+          <div className="flex justify-center items-center gap-2 md:gap-4 w-fit">
+            <p className="hidden text-slate-400">Sort</p>
+            <BarsArrowDownIcon className="w-5 h-5 text-orange-500" />
+          </div>
+        </div>
+        {/* post button */}
+        <button
+          // onClick={openModal}
+          className="flex justify-center items-center w-fit px-2 md:px-5 py-2 rounded-md text-slate-800 bg-orange-500 font-semibold"
+        >
+          <p className="hidden md:flex whitespace-nowrap">
+            Have something to say ?
+          </p>
+          <PencilIcon className="w-7 h-7 block md:hidden" />
+        </button>
+      </div>
       <PostLayout>
         {isLoading && cardSkeleton}
         {isSuccess &&
