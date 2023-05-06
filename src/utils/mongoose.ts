3e5ad8d8448147b1788fec8e3036ce5853f09627
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-05-06 21:50:46 +0200

    update mongoose connection

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,16 +1,26 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { connect, set } from "mongoose";
-const dbString =
-  process.env.DB_CONNECTION_STRING ||
-  "mongodb://localhost:27017/strangershub?retryWrites=true&w=majority";
+// import { connect, set } from "mongoose";
+// const dbString =
+//   process.env.DB_CONNECTION_STRING ||
+//   "mongodb://localhost:27017/strangershub?retryWrites=true&w=majority";
 
-// connection to db
-(async () => {
-  try {
-    set("strictQuery", false);
-    const db = await connect(dbString);
-    console.log("Db connectect to", db.connection.name);
-  } catch (error) {
-    console.error(error);
+// // connection to db
+// (async () => {
+//   try {
+//     set("strictQuery", false);
+//     const db = await connect(dbString);
+//     console.log("Db connectect to", db.connection.name);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
+
+import mongoose from "mongoose";
+
+export const connectMongoDB = async () => {
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection.asPromise();
   }
-})();
+
+  return await mongoose.connect(process.env.DB_CONNECTION_STRING || "");
+};
