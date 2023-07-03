6da9bd4bcc95da1da0b104d943441a17c2017925
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-07-03 14:13:59 +0200

    refactor post page

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,41 +1,45 @@
-import Seo from "@/components/SEO";
-import PostCardSkeleton from "@/components/postCardSkeleton";
-import axios from "axios";
-import {
-  ArrowSmallUpIcon,
-  BarsArrowDownIcon,
-  MagnifyingGlassIcon,
-  PencilIcon,
-} from "@heroicons/react/24/outline";
-import { useEffect, useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
-import Link from "next/link";
-import PostInputCard from "@/components/PostInputCard";
-import PostContainer from "@/components/PostContainer";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ArrowSmallUpIcon } from '@heroicons/react/24/outline';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+import Seo from '@/components/SEO';
+import PostCardSkeleton from '@/components/postCardSkeleton';
+import PostInputCard from '@/components/PostInputCard';
+import PostContainer from '@/components/PostContainer';
 
 const Post = () => {
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [prevSearchQuery, setPrevSearchQuery] = useState<string>("");
+  const [searchQuery, setSearchQuery] = useState('');
+  const [prevSearchQuery, setPrevSearchQuery] = useState('');
   const [posts, setPosts] = useState([]);
-  const [onSearchMode, setOnSearchMode] = useState<boolean>(false);
-  const [hasMorePost, setHasMorePost] = useState<boolean>(true);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isSuccess, setIsSuccess] = useState<boolean>(false);
-  const [pageIndex, setPageIndex] = useState<number>(1);
-  const [limit, setLimit] = useState<number>(40);
+  const [onSearchMode, setOnSearchMode] = useState(false);
+  const [hasMorePost, setHasMorePost] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [limit, setLimit] = useState(40);
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const cardSkeleton = [];
 
-  for (let i = 0; i < 12; i++) {
-    cardSkeleton.push(<PostCardSkeleton key={i} />);
-  }
+  const [showButton, setShowButton] = useState(false);
 
-  const isBrowser = () => typeof window !== "undefined";
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setShowButton(scrollY > 200);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   const scrollToTop = () => {
-    if (!isBrowser()) return;
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    const element = document.getElementById('to-the-top');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
   };
 
   const fetchLatestPost = () => {
@@ -43,77 +47,48 @@ const Post = () => {
       fetchSearchPost();
       return;
     }
-    if (pageIndex === 1) {
-      axios
-        .get(`${baseUrl}/api/v1/post?page=${pageIndex}&limit=${limit}`)
-        .then((res) => {
-          setIsLoading(false);
-          setPosts(res.data.posts);
-          setIsSuccess(true);
-        })
-        .catch(() => {
-          setIsLoading(false);
-        });
 
-      setPageIndex(pageIndex + 1);
-    } else {
-      axios
-        .get(`${baseUrl}/api/v1/post?page=${pageIndex}&limit=${limit}`)
-        .then((res) => {
-          if (res.data.posts.length === 0) {
-            setHasMorePost(false);
-          }
-          let postConcat = [...posts, ...res.data.posts];
-          setPosts(postConcat as any);
-        })
-        .catch(() => {
-          setPageIndex(1);
-        });
-      setPageIndex(pageIndex + 1);
-    }
+    const index = pageIndex === 1 ? pageIndex : pageIndex + 1;
+    axios
+      .get(`${baseUrl}/api/v1/post?page=${index}&limit=${limit}`)
+      .then((res) => {
+        setIsLoading(false);
+        setPosts((prevPosts) =>
+          pageIndex === 1 ? res.data.posts : [...prevPosts, ...res.data.posts]
+        );
+        setIsSuccess(true);
+        setPageIndex(index);
+        setHasMorePost(res.data.posts.length > 0);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setPageIndex(1);
+      });
   };
 
   const fetchSearchPost = () => {
     if (!searchQuery.trim()) {
       setOnSearchMode(false);
-      const index = 1;
-      setHasMorePost(true);
-      axios
-        .get(`${baseUrl}/api/v1/post?page=${index}&limit=${limit}`)
-        .then((res) => {
-          setIsLoading(false);
-          setPosts(res.data.posts);
-          setIsSuccess(true);
-          setPageIndex(index + 1);
-        })
-        .catch(() => {
-          setIsLoading(false);
-        });
-    } else {
-      setIsLoading(true);
       setPageIndex(1);
       setHasMorePost(true);
-      let index = onSearchMode ? pageIndex : 1;
-      prevSearchQuery != searchQuery && (index = 1);
+    } else {
+      setIsLoading(true);
+      setPageIndex((prevIndex) => (onSearchMode ? prevIndex : 1));
+      setHasMorePost(true);
+      const index = onSearchMode ? pageIndex : 1;
+      const url = `${baseUrl}/api/v1/post/search?query=${searchQuery}&page=${index}&limit=${limit}`;
 
       axios
-        .get(
-          `${baseUrl}/api/v1/post/search?query=${searchQuery}&page=${index}&limit=${limit}`
-        )
+        .get(url)
         .then((res) => {
-          let postConcat = onSearchMode
-            ? [...posts, ...res.data.posts]
-            : res.data.posts;
+          const postConcat = onSearchMode ? [...posts, ...res.data.posts] : res.data.posts;
 
-          prevSearchQuery != searchQuery && (postConcat = res.data.posts);
           setPrevSearchQuery(searchQuery);
           setOnSearchMode(true);
           setPageIndex(index + 1);
           setIsLoading(false);
-          if (res.data.posts.length === 0) {
-            setHasMorePost(false);
-          }
-          setPosts(postConcat as any);
+          setHasMorePost(res.data.posts.length > 0);
+          setPosts(postConcat);
         })
         .catch(() => {
           setIsLoading(false);
@@ -125,9 +100,8 @@ const Post = () => {
     fetchLatestPost();
   }, []);
 
-  // when to post modal is closed
   useEffect(() => {
-    !isModalOpen &&
+    if (!isModalOpen) {
       axios
         .get(`${baseUrl}/api/v1/post?page=${1}&limit=${limit}`)
         .then((res) => {
@@ -139,42 +113,41 @@ const Post = () => {
         .catch(() => {
           setIsLoading(false);
         });
+    }
   }, [isModalOpen]);
 
-  const handleChange = (e: any) => {
+  const handleChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
-  // on search submit
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     fetchSearchPost();
   };
 
+  for (let i = 0; i < 12; i++) {
+    cardSkeleton.push(<PostCardSkeleton key={i} />);
+  }
+
   return (
     <>
       <Seo
-        templateTitle={"Share Your Thoughts"}
-        description={
-          "Join us today and let your voice be heard - without ever having to reveal your identity!"
-        }
+        templateTitle="Share Your Thoughts"
+        description="Join us today and let your voice be heard - without ever having to reveal your identity!"
       />
-
+      <span id="to-the-top" />
       <main className="container p-2 mx-auto grid md:grid-cols-5 h-screen max-w-6xl">
-        <div className="">{/* right side */}</div>
+        <div>{/* right side */}</div>
         <div className="col-span-3 pt-20">
-          {/* Post input area */}
           <PostInputCard callback={fetchLatestPost} />
-          {/* space */}
           <div className="h-5" />
-          {/* post container */}
           <InfiniteScroll
             dataLength={posts.length}
             next={fetchLatestPost}
             hasMore={hasMorePost}
-            loader={""}
+            loader={null}
             endMessage={
-              <p style={{ textAlign: "center" }}>
+              <p style={{ textAlign: 'center' }}>
                 <b>Yay! You have seen it all</b>
               </p>
             }
@@ -182,16 +155,14 @@ const Post = () => {
             <PostContainer posts={posts} />
           </InfiniteScroll>
           <button
-            onClick={() => {
-              if (!isBrowser()) return;
-              window.scrollTo({ top: 0, behavior: "smooth" });
-            }}
-            className="fixed bg-orange-500 bottom-10 right-6 md:bottom-20 md:right-14 w-fit h-fit rounded-md z-40"
+            onClick={scrollToTop}
+            style={{ display: showButton ? 'block' : 'none' }}
+            className="fixed bottom-10 bg-orange-500 right-6 md:bottom-20 md:right-14 w-fit h-fit rounded-md z-40"
           >
             <ArrowSmallUpIcon className="w-6 h-6 m-3 text-slate-800" />
           </button>
         </div>
-        <div className="">{/* left side */}</div>
+        <div>{/* left side */}</div>
       </main>
     </>
   );
