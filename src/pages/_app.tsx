4acf80a3f167ec0426d7bfbf48b0aa7aa1a0d48f
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-07-06 01:44:11 +0200

    again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
